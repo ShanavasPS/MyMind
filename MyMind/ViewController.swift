f4948ebb4355a1529d22f520f806dc45039b12f1
Shanavas Shaji <shanavascruise@gmail.com>
2020-02-13 10:07:16 +0200

    Initial Commit

---
+++ MyMind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMind
+//
+//  Created by Shamnad PS on 13/02/20.
+//  Copyright © 2020 shanavas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

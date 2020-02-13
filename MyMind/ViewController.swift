84893cd89e333e10222264b4ae6e44515cec9641
Shanavas Shaji <shanavascruise@gmail.com>
2020-02-13 11:45:57 +0200

    Added Navigation bar

--- MyMind/ViewController.swift
+++ MyMind/ViewController.swift
@@ -12,9 +12,25 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        addNavBarImage();
     }
 
-
+    func addNavBarImage() {
+        let navController = navigationController!
+        
+        let image = #imageLiteral(resourceName: "Icon_navbar_Logo");
+        let imageView = UIImageView(image: image)
+        
+        let bannerWidth = navController.navigationBar.frame.size.width
+        let bannerHeight = navController.navigationBar.frame.size.height
+        
+        let bannerX = bannerWidth / 2 - image.size.width / 2
+        let bannerY = bannerHeight / 2 - image.size.height / 2
+        
+        imageView.frame = CGRect(x: bannerX, y: bannerY, width: bannerWidth - 20, height: bannerHeight - 20)
+        imageView.contentMode = .scaleAspectFit
+        
+        navigationItem.titleView = imageView
+    }
 }
 

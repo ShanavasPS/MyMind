773d849bd3ccca4e81523ad2ee7d368e9e8ab180
Shanavas Shaji <shanavascruise@gmail.com>
2020-02-15 12:12:25 +0200

    Moved channels fetch to WebServiceManager class

---
+++ MyMind/Model/WebServiceManager.swift
@@ -0,0 +1,57 @@
+//
+//  WebServiceManager.swift
+//  MyMind
+//
+//  Created by Shamnad PS on 15/02/20.
+//  Copyright © 2020 shanavas. All rights reserved.
+//
+
+import Foundation
+
+protocol WebServiceManagerDelegate {
+    func fetchChannelsSuccess();
+}
+
+class WebServiceManager {
+    var delegate: WebServiceManagerDelegate?
+    public static let sharedInstance = WebServiceManager()
+    var channels = Channels.sharedInstance;
+
+    init() {
+        
+    }
+    
+    public func fetchChannels() {
+        loadChannels();
+        delegate?.fetchChannelsSuccess();
+    }
+    
+    func loadChannels() {
+        channels.add(name: "FASHION", image: "Thumbnail_channel_Fashion", followers: "234k Followers", channelType: .explore, isFollowing: true);
+        channels.add(name: "SCIENCE", image: "Thumbnail_channel_Science", followers: "123k Followers", channelType: .popular, isFollowing: true);
+        channels.add(name: "AUTO", image: "Thumbnail_channel_Auto", followers: "100k Followers", channelType: .explore, isFollowing: true);
+        channels.add(name: "TECHNOLOGY", image: "Thumbnail_channel_Technology", followers: "23.6k Followers", channelType: .popular, isFollowing: true);
+        channels.add(name: "ENTERTAINMENT", image: "Thumbnail_channel_Entertainment", followers: "63k Followers", channelType: .explore, isFollowing: true);
+        channels.add(name: "ENVIRONMENT", image: "Thumbnail_channel_Environment", followers: "111k Followers", channelType: .popular, isFollowing: true);
+        channels.add(name: "FINANCES", image: "Thumbnail_channel_Finance", followers: "234k Followers", channelType: .explore, isFollowing: true);
+        channels.add(name: "TRAVEL", image: "Thumbnail_channel_Travel", followers: "422k Followers", channelType: .popular, isFollowing: true);
+        var index = 0;
+        channels.items.map({_ in
+            self.loadNews(index: index);
+            index += 1;
+            })
+    }
+    
+    func loadNews(index: Int) {
+        channels.items[index].add(title: "Artic sea ice extend hits record low for winter maximum", source: "CNN", time: "Apr 3, 2015");
+        channels.items[index].add(title: "Archeologists discovered lost city in Honduran jungle", source: "CNN", time: "Apr 3, 2015");
+        channels.items[index].add(title: "The balloons that could fly the tourists to the end of space", source: "CNN", time: "Apr 1, 2015");
+        channels.items[index].add(title: "In the Prawn Nebula, a Nursery for Newborn stars", source: "The New York Times", time: "March 30, 2015");
+        channels.items[index].add(title: "What will the battleship of the Future look like?", source: "Popular Science", time: "Apr 1, 2015");
+        channels.items[index].add(title: "Artic sea ice extend hits record low for winter maximum", source: "CNN", time: "Apr 3, 2015");
+        channels.items[index].add(title: "Archeologists discovered lost city in Honduran jungle", source: "CNN", time: "Apr 3, 2015");
+        channels.items[index].add(title: "The balloons that could fly the tourists to the end of space", source: "CNN", time: "Apr 1, 2015");
+        channels.items[index].add(title: "In the Prawn Nebula, a Nursery for Newborn stars", source: "The New York Times", time: "March 30, 2015");
+        channels.items[index].add(title: "What will the battleship of the Future look like?", source: "Popular Science", time: "Apr 1, 2015");
+    }
+}
--- MyMind/ViewController.swift
+++ MyMind/ViewController.swift
@@ -12,12 +12,15 @@ class ViewController: UICollectionViewController {
 
     private let reuseIdentifier = "ChannelCell"
     private var headerView:HomeHeaderView = HomeHeaderView();
+    private var webServiceManager = WebServiceManager.sharedInstance;
     var channels = Channels.sharedInstance;
     var channelList:[Channel] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         addNavBarImage();
-        loadChannels();
+        
+        webServiceManager.delegate = self;
+        webServiceManager.fetchChannels();
         
         navigationItem.backBarButtonItem = UIBarButtonItem(
             title: "",
@@ -33,36 +36,6 @@ class ViewController: UICollectionViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated);
         updateTableBasedonHeader(channels.selectedChannelType);
-    }
-    
-    func loadChannels() {
-        channels.add(name: "FASHION", image: "Thumbnail_channel_Fashion", followers: "234k Followers", channelType: .explore, isFollowing: true);
-        channels.add(name: "SCIENCE", image: "Thumbnail_channel_Science", followers: "123k Followers", channelType: .popular, isFollowing: true);
-        channels.add(name: "AUTO", image: "Thumbnail_channel_Auto", followers: "100k Followers", channelType: .explore, isFollowing: true);
-        channels.add(name: "TECHNOLOGY", image: "Thumbnail_channel_Technology", followers: "23.6k Followers", channelType: .popular, isFollowing: true);
-        channels.add(name: "ENTERTAINMENT", image: "Thumbnail_channel_Entertainment", followers: "63k Followers", channelType: .explore, isFollowing: true);
-        channels.add(name: "ENVIRONMENT", image: "Thumbnail_channel_Environment", followers: "111k Followers", channelType: .popular, isFollowing: true);
-        channels.add(name: "FINANCES", image: "Thumbnail_channel_Finance", followers: "234k Followers", channelType: .explore, isFollowing: true);
-        channels.add(name: "TRAVEL", image: "Thumbnail_channel_Travel", followers: "422k Followers", channelType: .popular, isFollowing: true);
-        var index = 0;
-        channels.items.map({_ in
-            self.loadNews(index: index);
-            index += 1;
-            })
-        channelList = channels.items;
-    }
-    
-    func loadNews(index: Int) {
-        channels.items[index].add(title: "Artic sea ice extend hits record low for winter maximum", source: "CNN", time: "Apr 3, 2015");
-        channels.items[index].add(title: "Archeologists discovered lost city in Honduran jungle", source: "CNN", time: "Apr 3, 2015");
-        channels.items[index].add(title: "The balloons that could fly the tourists to the end of space", source: "CNN", time: "Apr 1, 2015");
-        channels.items[index].add(title: "In the Prawn Nebula, a Nursery for Newborn stars", source: "The New York Times", time: "March 30, 2015");
-        channels.items[index].add(title: "What will the battleship of the Future look like?", source: "Popular Science", time: "Apr 1, 2015");
-        channels.items[index].add(title: "Artic sea ice extend hits record low for winter maximum", source: "CNN", time: "Apr 3, 2015");
-        channels.items[index].add(title: "Archeologists discovered lost city in Honduran jungle", source: "CNN", time: "Apr 3, 2015");
-        channels.items[index].add(title: "The balloons that could fly the tourists to the end of space", source: "CNN", time: "Apr 1, 2015");
-        channels.items[index].add(title: "In the Prawn Nebula, a Nursery for Newborn stars", source: "The New York Times", time: "March 30, 2015");
-        channels.items[index].add(title: "What will the battleship of the Future look like?", source: "Popular Science", time: "Apr 1, 2015");
     }
 
     func addNavBarImage() {
@@ -136,6 +109,13 @@ extension ViewController: HomeHeaderViewDelegate {
         }
         channels.selectedChannelType = type;
         self.collectionView.reloadData();
+    }
+}
+
+extension ViewController: WebServiceManagerDelegate {
+    func fetchChannelsSuccess() {
+        channelList = channels.items;
+        updateTableBasedonHeader(channels.selectedChannelType);
     }
 }
 
